2d15250e568941ba4ad8c1c13d09c2c4c92a0db4
Solidale <microsolid@outlook.de>
2025-05-29 20:20:27 +0200

    all_construct solved

---
+++ all_construct_tabulation.py
@@ -0,0 +1,21 @@
+
+def all_construct_tabulation(target: str, word_bank: list[str]) -> list[list[str]]:
+    table = [[] for i in range(len(target) + 1)]
+    table[0] = [[]]
+
+    for i in range(len(target)):
+        for word in word_bank:
+
+            if target[i: i + len(word)] == word:
+                for combination in table[i]:
+                    table[i + len(word)].append(combination + [word])
+    
+    print(table)
+    return len(table[len(target)])
+
+
+
+def test_all_construct_tabulation():
+    assert(all_construct_tabulation("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]) == 4)
+    assert(all_construct_tabulation("purple", ["purp", "p", "ur", "le", "purpl"]) == 2)
+    assert(all_construct_tabulation("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]) == 0)
